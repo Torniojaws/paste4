910d5ea22651ebd9a843318a50656965c2392a8b
Juha <torniojaws@gmail.com>
2018-07-23 09:47:35 +0300

    Implemented POST /login and tests (#26)
    Implemented POST /login with tests

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const pastes = require('./apps/pastes/controller');
+const login = require('./apps/login/controller');
 
 // Root
 app.get('/', (req, res) => res.status(200).json({ message: "Hello there" }));
@@ -13,5 +14,13 @@ app.route('/pastes/:id')
   .get(pastes.getPasteById)
   .put(pastes.editPaste)
   .delete(pastes.deletePaste);
+
+// Login / logout
+app.route('/login')
+  .post(login.doLogin);
+/*
+app.route('/logout')
+  .post(login.logout);
+*/
 
 module.exports = app;
